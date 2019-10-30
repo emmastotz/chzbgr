7caad1b81c2f81c69a6827202c0b7e1a59b71f63
Emma Stotz <stotz.emma@gmail.com>
2019-10-30 19:42:45 -0400

    Created database schema and seeds. Created server.js. Created reset.css.

---
+++ database/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ("Gourdon Hamsey");
+INSERT INTO burgers (burger_name) VALUES ("Don't You Four Cheddar 'Bout Me");
+INSERT INTO burgers (burger_name) VALUES ("If Looks Could Kale");
+INSERT INTO burgers (burger_name) VALUES ("Sweet Home Avocado");
+INSERT INTO burgers (burger_name) VALUES ("Eggers Can't Be Cheesers");
---
+++ server.js
@@ -0,0 +1,31 @@
+// ====================================================
+// DEPENDENCIES
+// ====================================================
+// Express
+var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json);
+// ====================================================
+// Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+// ====================================================
+// ROUTES
+// ====================================================
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+// ====================================================
+// LISTEN
+// ====================================================
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
+// ====================================================
